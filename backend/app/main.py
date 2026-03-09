92abee89b3735bb3e413d26ba57f91e3c62429d7
Ahmad Shahbaz <ahmadshahbaz@Exponentos-MacBook-Pro.local>
2026-03-09 06:49:26 +0500

    Added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from .db import engine, Base
 from .routers import tasks, categories
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(title="Task-Manager-API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=["localhost:5173"], allow_credentials=True,
+    allow_methods=['*'], allow_headers=["*"]
+)
 
 app.include_router(tasks.rt, prefix="/api/tasks", tags=["Tasks"])
 app.include_router(categories.rt, prefix="/api/categories", tags=["Categories"])
